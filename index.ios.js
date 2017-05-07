855a96090aebd26c801d1cd811b22942a15c2e88
Malek <mmalek1421@gmail.com>
2017-05-07 17:25:46 -0400

    added album titles

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,15 @@
 // Import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render to device
